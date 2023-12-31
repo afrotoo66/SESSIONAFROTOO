ec25ae51b84bc54a56e01899d24aed46e18ad1ec
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2023-12-31 03:45:29 +0200

    Update bot_users.py

--- StringGenBot/bot_users.py
+++ StringGenBot/bot_users.py
@@ -13,4 +13,4 @@ async def users_sql(_, msg: Message):
 @Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = len(await get_served_users())
-    await msg.reply_text(f"⎊ الإحصائيات الحالية لبوت جلسات سبايدر :\n\n {users} ᴜsᴇʀs", quote=True)
+    await msg.reply_text(f" الإحصائيات الحالية لبوت جلسات عفرتو :\n\n {users} ᴜsᴇʀs", quote=True)
